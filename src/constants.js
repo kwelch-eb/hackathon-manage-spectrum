5b6721312260938026bc9c92ace31750d86f5fb5
Kyle Welch <kwelch@eventbrite.com>
2018-11-02 14:32:26 -0500

    fixed default constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://eventbrite.com';
-export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL || 'https://www.evbqaapi.com/v3/';
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL || 'https://www.eventbriteapi.com/v3/';
 export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
