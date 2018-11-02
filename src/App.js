bd92b57af59f7d468656c501d49462a1ec185cc7
Kyle Welch <kwelch@eventbrite.com>
2018-11-02 15:45:15 -0500

    pass sdk as prop to pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { RegisterPage, HomePage, ExtensionPage } from './pages';
+
+import { parse } from 'query-string';
+import jwt from 'jsonwebtoken';
+import eventbrite from 'eventbrite';
+import { BASE_API_URL, CLIENT_SECRET } from './constants';
+
 import(/* webpackPreload: true */'eventbrite_design_system/css/eds.css');
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const query = parse(window.location.search || '');
+    const token = query.esr ? jwt.decode(query.esr, CLIENT_SECRET) : null;
+    this.state = {
+      ...token,
+    };
+    this.sdk = eventbrite({ baseUrl: BASE_API_URL, token: token.auth_token });
+  }
+
+  withSDK = (Component) => (routeProps) => (<Component {...routeProps} sdk={this.sdk} />);
+
   render() {
     return (
       <Router>
         <div className="App">
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/extension" component={ExtensionPage} />
-          <Route exact path="/"  component={HomePage} />
+          <Route path="/register" component={this.withSDK(RegisterPage)} />
+          <Route path="/extension" component={this.withSDK(ExtensionPage)} />
+          <Route exact path="/"  component={this.withSDK(HomePage)} />
         </div>
       </Router>
     );
--- src/pages/ExtensionPage.js
+++ src/pages/ExtensionPage.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { parse } from 'query-string';
-import jwt from 'jsonwebtoken';
-import eventbrite from 'eventbrite';
-import { BASE_API_URL, CLIENT_SECRET } from '../constants';
 
 export default class ExtensionPage extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    const query = parse(props.location.search || '');
-    const token = query.esr ? jwt.decode(query.esr, CLIENT_SECRET) : null;
-    this.state = {
-      ...token,
-    };
-    this.sdk = eventbrite({ baseUrl: BASE_API_URL, token: token.auth_token });
-  }
+  state = {
+    user: null,
+  };
 
   componentDidMount() {
-    this.sdk.request('/users/me/').then(user => this.setState({ user }));
+    this.props.sdk.request('/users/me/').then(user => this.setState({ user }));
     // this isn't working so it has scroll :sad_panda:
     // window.EB.FrameAPI.init({});
   }
